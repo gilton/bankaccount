40b100919ce7e85144d2c79c632812db204b3850
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2024-04-16 12:42:48 -0300

    fix: the cardinality of the relationships

--- src/main/java/org/labs/bank/banksling/models/Banco.java
+++ src/main/java/org/labs/bank/banksling/models/Banco.java
@@ -6,11 +6,10 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -27,10 +26,10 @@ public class Banco implements Serializable {
 	@Column(name = "nom_banco")
 	private String nomBanco;
 	
-	@ManyToOne(fetch = FetchType.LAZY, targetEntity = Conta.class)
+	@OneToMany(mappedBy = "idConta", orphanRemoval = true)
 	private List<Conta> contas;
 	
-	@ManyToOne(fetch = FetchType.LAZY, targetEntity = Cliente.class)
+	@OneToMany(mappedBy = "idCliente", orphanRemoval = true)
 	private List<Cliente> clientes;
 
 	
