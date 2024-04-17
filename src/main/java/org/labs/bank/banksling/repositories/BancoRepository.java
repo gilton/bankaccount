35d1c57376f029e921e5db142cc32febdd8e2318
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2024-04-17 15:48:15 -0300

    feat: add for banco entity -> respository controller record-dto

--- src/main/java/org/labs/bank/banksling/repositories/BancoRepository.java
+++ src/main/java/org/labs/bank/banksling/repositories/BancoRepository.java
@@ -1,11 +1,16 @@
 package org.labs.bank.banksling.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.labs.bank.banksling.models.Banco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BancoRepository extends JpaRepository<Banco, UUID> {
+
+	@Query(value = "FROM Banco b WHERE b.codigoBanco = ?1")
+	Optional<Banco> findByCodigo(Integer codigoBanco);
 }
