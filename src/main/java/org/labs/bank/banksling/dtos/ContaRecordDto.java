cd1412ab1d464d41699c264824cb3903674ba870
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2024-04-25 10:34:18 -0300

    feat: add tipoConta attribute

--- src/main/java/org/labs/bank/banksling/dtos/ContaRecordDto.java
+++ src/main/java/org/labs/bank/banksling/dtos/ContaRecordDto.java
@@ -2,10 +2,12 @@ package org.labs.bank.banksling.dtos;
 
 import org.labs.bank.banksling.models.Banco;
 import org.labs.bank.banksling.models.Cliente;
+import org.labs.bank.banksling.models.enums.TipoConta;
 
 public record ContaRecordDto(
 		Long numeroConta,
 		Integer agencia,
+		TipoConta tipoConta,
 		Double saldo,
 		Banco banco,
 		Cliente cliente
